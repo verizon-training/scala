f2393fedf011604eb686361836f06aeedc10211a
verizon01 <verizon01@iiht.tech>
2021-12-06 16:26:46 +0530

    matching with case classes

--- basics/src/day2/MatcherWIthCasClass.scala
+++ basics/src/day2/MatcherWIthCasClass.scala
@@ -1,5 +1,28 @@
 package day2
 
+abstract class Notification
+case class Email (sender:String, title:String, body:String ="") extends Notification
+case class SMS (sender:String, title:String) extends Notification
+case class Voice (sender:String, title:String, body:String) extends Notification
 object MatcherWIthCasClass {
   
+  def main(args: Array[String]): Unit = {
+    def showNotification(notification : Notification):String = {
+      println(notification)
+      notification match {
+        case Email(sender, title, _) => 
+          s"you have got email "
+        case SMS(sender, title) => 
+          s"you have got EMS"
+        case Voice(sender, title, _) => 
+          s"you have got voice message"
+      }
+    }
+    
+    val sms1 = SMS("983838383", "Are you fine")
+    val email = Email("983838383", "Are you fine")
+    println(showNotification(sms1))
+    println(showNotification(email))
+    
+  }
 }
